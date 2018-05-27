ee28910b729d2e1f0d82a9c3bdf08035d3d896dc
wujinwen <jwwu@gizwits.com>
2018-05-27 21:50:17 +0800

    新增：用户模型

--- user_center/models.py
+++ user_center/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+
+class User(auth_models.AbstractUser):
+    
+    avatar = models.ImageField('头像')
+    wx_nickname = models.CharField('微信昵称', max_length=32, blank=True, null=True)
+    nickname = models.CharField('昵称', max_length=32, blank=True, null=True) 
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
